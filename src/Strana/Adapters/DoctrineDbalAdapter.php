6faa5d354cac7ff040815cdf0f9db093e4625620
Muhammad Usman <mdusyl@yahoo.com>
2013-11-10 00:45:06 +0600

    Added DoctrineDbal adapter.

---
+++ src/Strana/Adapters/DoctrineDbalAdapter.php
@@ -0,0 +1,44 @@
+<?php namespace Strana\Adapters;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use Strana\ConfigHelper;
+use Strana\Interfaces\CollectionAdapter;
+
+class DoctrineDbalAdapter implements CollectionAdapter{
+
+    /**
+     * @var \Strana\ConfigHelper
+     */
+    protected $configHelper;
+
+    /**
+     * @var QueryBuilder
+     */
+    protected $records;
+
+    public function __construct($records, ConfigHelper $configHelper)
+    {
+        if (! $records instanceof QueryBuilder) {
+            throw new \InvalidArgumentException;
+        }
+
+        $this->records = $records;
+        $this->configHelper = $configHelper;
+    }
+
+    public function slice()
+    {
+        $records = clone($this->records);
+        $limit = $this->configHelper->getLimit();
+        $offset = $this->configHelper->getOffset();
+        return $records->setMaxResults($limit)->setFirstResult($offset)->execute()->fetchAll();
+    }
+
+    public function total()
+    {
+        $records = clone($this->records);
+        $records->select('count(*) as cnt');
+        $count = $records->execute()->fetchColumn();
+        return $count;
+    }
+}
