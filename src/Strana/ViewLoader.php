d74fd4e49c42b81c248c6288d0fa688729b9b6da
Muhammad Usman <mdusyl@yahoo.com>
2013-11-12 19:06:19 +0600

    Replaced ViewLoader code with safer one.

--- src/Strana/ViewLoader.php
+++ src/Strana/ViewLoader.php
@@ -3,21 +3,48 @@
 use Strana\Exceptions\Exception;
 
 class ViewLoader {
-    public function load($path, Array $data = array())
+    /**
+     * @param $file_path
+     * @param array $data
+     * @return mixed
+     * @throws Exceptions\Exception
+     *
+     * Process view files.
+     *
+     * Credit: FuelPHP https://github.com/fuel/core/blob/1.8/develop/classes/view.php#L228
+     */
+    public function load($file_path, Array $data = array())
     {
-        $path = dirname(__FILE__) . '/Views/' . $path;
+        $file_path = dirname(__FILE__) . '/Views/' . $file_path;
 
-        if (!is_readable($path) ) {
-            throw new Exception('View not found ' . $path . '.');
+        if (!is_readable($file_path) ) {
+            throw new Exception('View not found ' . $file_path . '.');
         }
 
-        $viewData = file_get_contents($path);
+        $clean_room = function($__file_name, array $__data)
+        {
+            extract($__data, EXTR_REFS);
 
-        ob_start() && extract($data, EXTR_SKIP);
-        eval('?>'.$viewData);
-        $viewData = ob_get_clean();
-        ob_flush();
+            // Capture the view output
+            ob_start();
 
-        return $viewData;
+            try
+            {
+                // Load the view within the current scope
+                require $__file_name;
+            }
+            catch (\Exception $e)
+            {
+                // Delete the output buffer
+                ob_end_clean();
+
+                // Re-throw the exception
+                throw $e;
+            }
+
+            // Get the captured output and close the buffer
+            return ob_get_clean();
+        };
+        return $clean_room($file_path, $data);
     }
 }
