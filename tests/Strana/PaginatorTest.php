4bb167193c0285d91ac30e16dd9b324b6d4fbaa4
Muhammad Usman <mdusyl@yahoo.com>
2013-11-14 00:31:30 +0600

    Exceptions testing.

--- tests/Strana/PaginatorTest.php
+++ tests/Strana/PaginatorTest.php
@@ -54,7 +54,7 @@ class PaginatorTest extends TestCase
             $records['Key ' . $i] = 'Value ' . $i;
         }
 
-        $paginator = $paginatorClass->page(4)->perPage(10)->make($records);
+        $paginator = $paginatorClass->page(4)->perPage(10)->make($records, 'Array');
 
         // Cover foreach, iterator
         foreach($paginator as $item) {
@@ -63,5 +63,23 @@ class PaginatorTest extends TestCase
 
         $expected = '<ul class="pagination"><li class="prev"><a href="?page=3">&laquo;</a></li><li class=""><a href="?page=2">2</a></li><li class=""><a href="?page=3">3</a></li><li class="active current"><a href="?page=4">4</a></li><li class=""><a href="?page=5">5</a></li><li class=""><a href="?page=6">6</a></li><li class="next"><a href="?page=5">&raquo;</a></li></ul>';
         $this->assertEquals($expected, (string)$paginator);
+    }
+
+    /**
+     * @expectedException Strana\Exceptions\InvalidArgumentException
+     */
+    public function testAdapterNotFoundException()
+    {
+        $paginatorClass = new Paginator();
+        $paginatorClass->make(array(), 'Foo');
+    }
+
+    /**
+     * @expectedException Strana\Exceptions\InvalidArgumentException
+     */
+    public function testWithInvalidAdapter()
+    {
+        $paginatorClass = new Paginator();
+        $paginatorClass->make(array(), new \stdClass());
     }
 }
